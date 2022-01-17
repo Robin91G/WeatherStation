127640f485be476bb1ee272f5922b304f42b0c96
Robin <57486784+Robin91G@users.noreply.github.com>
2022-01-17 18:04:54 +0100

    Definition n° pin and configuration pin as input or output (indoor)

--- src/Program/Weather_Station_Indoor/Weather_Station_Indoor.ino
+++ src/Program/Weather_Station_Indoor/Weather_Station_Indoor.ino
@@ -1,6 +1,42 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RTClib.h>
+#include "dht.h"
+#include <MQ2.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+//Definition Digital Pin
+#define pinButton1 2
+#define pinButton2 3
+#define pinLedReceivedData 4
+#define pinLedRed 5
+#define pinLedGreen 6
+#define pin_radio_CE 7
+#define pin_radio_CSN 8
+#define pinSensorDTH22 9
+#define pinBuzzer 10
+#define pinButtonOledReset 14 //A0
+#define pinDigitalSensorMq2  15 //A1
+
+//Definition Analog Pin
+#define pinSensorMq2 A2
+
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  pinMode(pinButton1, INPUT);
+  pinMode(pinButton2, INPUT);
+  pinMode(pinLedReceivedData, OUTPUT);
+  pinMode(pinLedRed, OUTPUT);
+  pinMode(pinLedGreen, OUTPUT);
+  pinMode(pinSensorDTH22, INPUT);
+  pinMode(pinBuzzer, OUTPUT);
+  pinMode(pinDigitalSensorMq2, INPUT);
 }
 
 void loop() {
